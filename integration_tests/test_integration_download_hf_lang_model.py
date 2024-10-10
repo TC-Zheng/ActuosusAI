3206ceef8babb28275e172bba79f20ad1f342f94
TC-Zheng <eric.tc.zheng@gmail.com>
2024-10-10 17:56:43 -0400

    fix: Modify integration test to work with Github Action

--- integration_tests/test_integration_download_hf_lang_model.py
+++ integration_tests/test_integration_download_hf_lang_model.py
@@ -1,3 +1,6 @@
+import os
+from pathlib import Path
+
 from actuosus_ai.ai_model_manager.connection import get_async_db_session
 
 import shutil
@@ -8,19 +11,29 @@ from transformers import AutoModel, AutoTokenizer
 from actuosus_ai.ai_model_manager.language_model_service import LanguageModelService
 from integration_tests.integration_test_settings import get_test_settings
 
+DB_URL = "test.db"
+BASE_STORAGE_PATH = "test"
+MODEL_NAME_1 = "dslim/distilbert-NER"
 
 class TestIntegrationAiModelManager:
     @classmethod
     def setup_class(cls):
         # clean up test directory
-        shutil.rmtree("/home/zhengt/Projects/test", ignore_errors=True)
+        shutil.rmtree(BASE_STORAGE_PATH, ignore_errors=True)
+        Path(DB_URL).unlink(missing_ok=True)
+
+    @classmethod
+    def teardown_class(cls):
+        # clean up test directory
+        shutil.rmtree(BASE_STORAGE_PATH, ignore_errors=True)
+        Path(DB_URL).unlink(missing_ok=True)
 
     # Test by downloading a small bert model from hugging face then try to load it and check if it is saved in the database
     @pytest.mark.asyncio
     async def test_integration_download_hf_lang_model(self, client):
         # Arrange
 
-        payload = {"hf_model_id": "dslim/distilbert-NER"}
+        payload = {"hf_model_id": MODEL_NAME_1}
 
         # Act
         response = client.post("/download/hf_lang_model/", json=payload)
@@ -32,16 +45,16 @@ class TestIntegrationAiModelManager:
         }
         assert response.status_code == 200
         # Try to load the models
-        AutoModel.from_pretrained("/home/zhengt/Projects/test/dslim/distilbert-NER")
-        AutoTokenizer.from_pretrained("/home/zhengt/Projects/test/dslim/distilbert-NER")
+        AutoModel.from_pretrained(os.path.join(BASE_STORAGE_PATH, MODEL_NAME_1))
+        AutoTokenizer.from_pretrained(os.path.join(BASE_STORAGE_PATH, MODEL_NAME_1))
         # Check if the model info is saved in the database
         async for session in get_async_db_session(get_test_settings()):
             language_model_service = LanguageModelService(session)
             dtos = await language_model_service.get_all_models()
             assert len(dtos) == 1
             assert dtos[0].lm_id == 1
-            assert dtos[0].name == "dslim/distilbert-NER"
+            assert dtos[0].name == MODEL_NAME_1
             assert (
                 dtos[0].storage_path
-                == "/home/zhengt/Projects/test/dslim/distilbert-NER"
+                == os.path.join(BASE_STORAGE_PATH, MODEL_NAME_1)
             )
