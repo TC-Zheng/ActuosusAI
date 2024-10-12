27591ad1759dab54cd116183ed2b069e16dfe0c3
TC-Zheng <eric.tc.zheng@gmail.com>
2024-10-12 11:31:21 -0400

    Add next.js client

---
+++ client/tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from "tailwindcss";
+
+const config: {
+  plugins: any[];
+  theme: { extend: { colors: { background: string; foreground: string } } };
+  content: string[]
+} = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
